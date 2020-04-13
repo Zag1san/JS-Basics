bbd81bb0310c0b3a5e6e7b14b6093296aa191bb0
Zag1san <gogo_san@abv.bg>
2020-04-13 19:45:03 +0300

    Basic Syntax

--- First steps/FUNDAMENTALS/LECTION01-BasicSyntax/12MultiplicationTable.js
+++ First steps/FUNDAMENTALS/LECTION01-BasicSyntax/12MultiplicationTable.js
@@ -1,6 +1,6 @@
 function solve(number) {
 
-    for(let i = 1; i < 10; i++){
+    for(let i = 1; i <= 10; i++){
         product = number * i;
         console.log(`${number} X ${i} = ${product}`);
         
